2512b03b8280d771b659cb122e4649dd0ffcfe7f
DavidJoao <davidsandoval596@gmail.com>
2023-04-05 21:08:11 -0700

    CREATE mapping for users in settings

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,11 +1,13 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import AuthContext from '@/custom/AuthProvider'
 import CheckSession from '@/custom/CheckSession'
 import { useRouter } from 'next/router'
+import axios from '@/custom/axios'
 
 const settings = () => {
 
   const { auth, setAuth } = useContext(AuthContext)
+  const [users, setUsers] = useState([])
   const router = useRouter()
 
   useEffect(() => {
@@ -13,10 +15,26 @@ const settings = () => {
     if (!auth.token) router.push('/login')
   }, [])
 
+  useEffect(() => {
+    axios.get('/api/user/all')
+      .then(res => setUsers(res.data))
+  }, [])
+
 
   return (
-    <div>
-        <h1>Settings</h1>
+    <div className='border-[1px] border-black h-[700px] flex flex-col'>
+        <div className='h-1/2 flex flex-row items-center p-4 overflow-auto'>
+          { users && users.map(user => {
+            return(
+              <p className='form m-2 w-[200px]'>{user.name}</p>
+            )
+          })}
+        </div>
+        <div className='h-1/2 flex flex-row items-center p-4 overflow-auto'>
+          <p>one</p>
+          <p>one</p>
+          <p>one</p>
+        </div>
     </div>
   )
 }
