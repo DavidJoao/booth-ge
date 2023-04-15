6226d4f8d00bdf784c72c1093dee9bc88991d1b2
DavidJoao <davidsandoval596@gmail.com>
2023-04-14 20:23:25 -0700

    FIX backend route for adding and checking for occupied equipment

--- src/pages/api/jobsite/equipment/[equipmentId]/[jobsiteId].js
+++ src/pages/api/jobsite/equipment/[equipmentId]/[jobsiteId].js
@@ -7,19 +7,19 @@ export default async function (req, res, next) {
 
         const foundEquipment = await Equipment.findOne({ _id: req.query.equipmentId })
         const foundJobsite = await Jobsite.findOne({ _id: req.query.jobsiteId })
-        const existingEquipment = await foundJobsite.employees.find(name => name === foundEquipment.name)
-        const occupiedJob = await Jobsite.findOne({ equipment: foundEquipment.name })
+        const existingEquipment = await foundJobsite.employees.find(name => name === `${foundEquipment.number} ${foundEquipment.name}`)
+        const occupiedJob = await Jobsite.findOne({ equipment: `${foundEquipment.number} ${foundEquipment.name}` })
 
         if (!foundEquipment && !foundJobsite) {
             res.status(401).json( {message: 'Equipment not found'})
             return
         }
         if (existingEquipment !== undefined) {
-            res.status(401).json( {message:`${foundEquipment.name} already at this jobsite`})
+            res.status(401).json( {message:`${foundEquipment.number} ${foundEquipment.name} already at this jobsite`})
             return
         }
         if (occupiedJob) {
-            res.status(401).json( {message:`${foundEquipment.name} already at ${occupiedJob.name}`})
+            res.status(401).json( {message:`${foundEquipment.number} ${foundEquipment.name} already at ${occupiedJob.name}`})
             return
         }
         
