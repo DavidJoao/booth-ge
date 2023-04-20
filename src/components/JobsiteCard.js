f0c9ece879c07061e3f1c969fb0a9cc4e67ad351
DavidJoao <davidsandoval596@gmail.com>
2023-04-19 20:03:03 -0700

    CREATE Route and functionality to remove accessories from JobsiteCard and Equipment

--- src/components/JobsiteCard.js
+++ src/components/JobsiteCard.js
@@ -80,7 +80,17 @@ const JobsiteCard = ( { jobsite, auth } ) => {
                   <ul className='list-disc'>
                   {equipment.accessories.map(accessory => {
                     return(
-                      <li className='m-1'>{accessory.name}<button className='float-right bg-red-600 hover:bg-red-500 w-[20px] rounded text-center'>-</button></li>
+                      <li className='m-1'>{accessory.name}
+                      {auth.isAdmin ? 
+                        <button className='float-right bg-red-600 hover:bg-red-500 w-[20px] rounded text-center' onClick={(e) => {
+                          e.preventDefault()
+                          axios.patch(`/api/accessory/remove/${accessory._id}/${equipment._id}`)
+                            .then(res => loadAll())
+                        }}>-</button>
+                        :
+                        <></>
+                      }
+                      </li>
                     )
                   })}
                   </ul>
--- src/pages/api/accessory/remove/[accessoryId]/[equipmentId].js
+++ src/pages/api/accessory/remove/[accessoryId]/[equipmentId].js
@@ -1,3 +1,31 @@
+const Jobsite = require('../../../../../models/jobsite')
+const Equipment = require('../../../../../models/equipment')
+const Accessory = require('../../../../../models/accessory')
+
 export default async function (req, res, next) {
+    const foundEquipment = await Equipment.findById(req.query.equipmentId)
+    const foundAccessory = await Accessory.findById(req.query.accessoryId)
+    const foundJobsite = await Jobsite.findOne({ "equipment._id": foundEquipment._id })
+
+    if (!foundEquipment && !foundJobsite) throw new Error
+    const accessoryIndex = foundEquipment.accessories.findIndex((e) => e._id.equals(foundAccessory._id))
+    const equipmentIndex = foundJobsite.equipment.findIndex((e) => e._id.equals(foundEquipment._id))
+
+    // DELETE EQUIPMENT FROM JOBSITE
     
+    
+    
+    try {
+        if (equipmentIndex > -1) foundJobsite.equipment.splice(equipmentIndex, 1)
+        if (accessoryIndex > -1) foundEquipment.accessories.splice(accessoryIndex, 1)
+        
+        await foundJobsite.equipment.push(foundEquipment);
+        
+        await foundEquipment.save()
+        await foundJobsite.save()
+
+        res.json(foundEquipment)
+    } catch (error) {
+        next(error)
+    }
 }
