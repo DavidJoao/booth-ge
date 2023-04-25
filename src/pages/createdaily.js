2cc57da38f2eda3368998e6b27e131dc452bd5ec
DavidJoao <davidsandoval596@gmail.com>
2023-04-24 20:46:08 -0700

    CREATE Page for createdaily

---
+++ src/pages/createdaily.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const createdaily = () => {
+  return (
+    <div className="bg-[#242526] min-h-screen h-auto lg:h-screen flex flex-col items-center justify-center">
+        <div id='dropdown' className='w-[300px] w-[70%] h-[600px] p-2 rounded'>
+
+        </div>
+    </div>
+  )
+}
+
+export default createdaily
