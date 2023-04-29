ca0ad6cc7e687b001272ecf3df0e94b735c932da
DavidJoao <davidsandoval596@gmail.com>
2023-04-29 12:38:39 -0700

    FIX bug of crash when deleting users that don't have jobsite assigned

--- src/pages/api/user/delete/[userId].js
+++ src/pages/api/user/delete/[userId].js
@@ -6,11 +6,15 @@ export default async function deleteUser (req, res, next) {
     try {
         const foundUser = await User.findOne({ _id: req.query.userId })
         const foundJobsite = await Jobsite.findOne({ "employees.name": foundUser.name })
-        const index = foundJobsite.employees.findIndex((e) => e._id.equals(req.query.userId))
-        if (index > -1) foundJobsite.employees.splice(index, 1)
-        await foundJobsite.save()
-        const deletedUser = await User.findOneAndDelete({ _id: req.query.userId})
-        res.json(deletedUser)
+        if (foundJobsite) {
+            const index = foundJobsite.employees.findIndex((e) => e._id.equals(req.query.userId))
+            if (index > -1) foundJobsite.employees.splice(index, 1)
+            await foundJobsite.save()
+            User.findOneAndDelete({ _id: req.query.userId})
+        } else {
+            const deletedUser = await User.findOneAndDelete({ _id: req.query.userId})
+            res.json(deletedUser)
+        }
     } catch (err) {
         console.error(err)
         res.status(500).send('Server Error')
