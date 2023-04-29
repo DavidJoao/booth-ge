ea9b31a06a386e467dee03d621dea69675820fdc
DavidJoao <davidsandoval596@gmail.com>
2023-04-28 20:08:18 -0700

    EDIT nextconfig file

--- next.config.js
+++ next.config.js
@@ -1,29 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: async () => {
-    return [
-      {
-        // matching all API routes
-        source: "/api/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ]
-  }
 }
 
 
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Allow all API routes to be accessed from any origin
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
+  nextConfig
+}
