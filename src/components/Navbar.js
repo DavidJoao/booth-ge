05a7a62723dbb6b37267376265269d15e28ea1b5
DavidJoao <davidsandoval596@gmail.com>
2023-05-03 18:08:01 -0700

    FIX Autocomplete error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,8 +30,8 @@ const Navbar = () => {
   return (
     <div className='p-2 border-[1px] border-black flex flex-row items-center justify-between bg-[#3a3b3c] text-white'>
 		<div className='w-[300px] lg:w-[800px] flex items-center'>
-			<Link href='/' className='w-full block lg:hidden text-white text-3xl no-underline'>Booth Grading</Link>
-			<Link href='/' className='w-1/2 hidden lg:block text-white text-3xl no-underline'>Booth Grading and Excavating</Link>
+			<Link href='/home' className='w-full block lg:hidden text-white text-3xl no-underline'>Booth Grading</Link>
+			<Link href='/home' className='w-1/2 hidden lg:block text-white text-3xl no-underline'>Booth Grading and Excavating</Link>
 			{ tokenCookie ? <img src="https://i.ibb.co/XJKs479/boothimg.jpg"  className='m-2 rounded-xl w-[50px]'/> : <></>}
 		</div>
     { tokenCookie ? 
