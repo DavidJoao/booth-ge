deb41a4be239a6838ed9b8880155529d4c4abef7
DavidJoao <davidsandoval596@gmail.com>
2023-07-08 23:55:23 -0700

    CREATE equipmentstatus page, working on displaying current equipment in yard

---
+++ src/pages/equipmentstatus.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CCSection from '@/components/CCSection'
+
+const EquipmentStatus = () => {
+  return (
+    <div className='flex flex-col items-center bg-[#242526] h-[1200px] lg:h-screen pt-[85px] pb-2'>
+
+    </div>
+  )
+}
+
+export default EquipmentStatus
