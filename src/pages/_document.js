147faf7602eadba8f4ff04232d54ea4534bcbe75
DavidJoao <davidsandoval596@gmail.com>
2023-09-18 10:16:44 -0700

    CHANGE Imported Google Font in head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,9 +5,12 @@ export default function Document() {
       <Html lang="en">
         <Head>
           <link rel='shortcut icon' href='https://cdn-icons-png.flaticon.com/512/1624/1624003.png'/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500&display=swap" rel="stylesheet"/>
         </Head>
         <title>Booth Grading and Excavating</title>
-        <body>
+        <body className='main-body'>
           <Main />
           <NextScript />
         </body>
