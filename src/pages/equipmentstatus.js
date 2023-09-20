5334ea3c669c3bcf32db9971e5190b0289b83127
DavidJoao <davidsandoval596@gmail.com>
2023-09-20 13:31:03 -0700

    ADD Front end for equipment reports

--- src/pages/equipmentstatus.js
+++ src/pages/equipmentstatus.js
@@ -1,10 +1,13 @@
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import CCSection from '@/components/CCSection'
 import AuthContext from '@/custom/AuthProvider'
+import CheckSession from '@/custom/CheckSession'
+import { PuffLoader } from 'react-spinners';
+import axios from '@/custom/axios';
 
 const EquipmentStatus = () => {
 
-  const { auth, equipment } = useContext(AuthContext)
+  const { auth, equipment, loadAll, setAuth } = useContext(AuthContext)
 
   const initialReport = {
     author: auth?.name,
@@ -14,7 +17,15 @@ const EquipmentStatus = () => {
   }
 
   const [report, setReport] = useState(initialReport)
+  const [isLoading, setIsLoading] = useState(false)
+  const [statusMessage, setStatusMessage] = useState('Submitting Equipment Report, Please Wait...');
 
+  useEffect(() => {
+    CheckSession(AuthContext, setAuth)
+      .then(() => {
+        loadAll()
+        })
+  }, [])
 
   const handleChange = (e) => {
     const {value, name} = e.target;
@@ -25,30 +36,57 @@ const EquipmentStatus = () => {
     })
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setIsLoading(true)
+    setStatusMessage('Submitting Report, Please Wait...')
+
+    axios.post('/api/email/report', report, { headers: { 'Content-Type': 'application/json ' }})
+      .then(res => {
+        setStatusMessage('✓ Equipment Report Submitted Successfully ✓')
+        setReport(initialReport)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, "2000");
+    })
+    .catch(err => {
+        console.log(err)
+        setIsLoading(false)
+    })
+  }
+
   return (
     <div className='flex flex-col items-center bg-[#242526] h-[1200px] lg:h-screen pt-[85px] pb-2'>
-      <form className='flex flex-col w-[350px] sm:w-full lg:w-3/4 bg-[#494A4C] p-4 text-white rounded-lg shadow-xl'>
-        <label className='font-extrabold text-xl mb-5'>Booth Grading and Excavating Equipment Report</label>
-        <div className='rounded-lg w-full sm:w-[600px] mx-auto mb-5 flex flex-col items-center justify-center'>
-          <label>Equipment:</label>
-          <select name='equipment' className='input w-full' onChange={handleChange}>
-            <option>Select Equipment:</option>
-            {/* EQUIPMENT MAPPING */}
-            { equipment && equipment.map(equipment => {
-              return (
-                <option key={`${equipment.number}${equipment.name}`}>{equipment.number} {equipment.name}</option>
-              )
-            }) }
-          </select>
-          <label>Foreman Description:</label>
-          <textarea className='input h-[200px] w-full' name='description' onChange={handleChange}>
-
-          </textarea>
-          <button className='buttons mt-3 w-full'>Submit</button>
+      { isLoading ? (
+        <div className='h-screen bg-[#242526] flex flex-col items-center justify-center'>
+            <PuffLoader color='#ffffff' loading={isLoading} size={120}/>
+            <p className="mt-4">{statusMessage}</p>
         </div>
-        <label>Report by {auth?.name}</label>
-        <label>Date: {Date().split(' ').splice(1, 3).join(' ')}</label>
-      </form>
+      ) : (
+        <form className='flex flex-col w-[350px] sm:w-full lg:w-3/4 bg-[#494A4C] p-4 text-white rounded-lg shadow-xl' onSubmit={handleSubmit}>
+          <label className='font-extrabold text-xl mb-5'>Booth Grading and Excavating Equipment Report</label>
+          <div className='rounded-lg w-full sm:w-[600px] mx-auto mb-5 flex flex-col items-center justify-center'>
+            <label>Equipment:</label>
+            <select name='equipment' value={report.equipment} className='input w-full' onChange={handleChange}>
+              <option>Select Equipment:</option>
+              {/* EQUIPMENT MAPPING */}
+              { equipment && equipment.map(equipment => {
+                return (
+                  <option key={`${equipment.number}${equipment.name}`}>{equipment.number} {equipment.name}</option>
+                )
+              }) }
+            </select>
+            <label>Foreman Description:</label>
+            <textarea className='input h-[200px] w-full' name='description' value={report.description} onChange={handleChange}>
+
+            </textarea>
+            <button type='submit' className='buttons mt-3 w-full'>Submit</button>
+          </div>
+          <label>Report by {auth?.name}</label>
+          <label>Date: {Date().split(' ').splice(1, 3).join(' ')}</label>
+        </form>
+      )}
         <CCSection />
     </div>
   )
