cec02a7e30512b1122666cb8b421f5550b3eab9d
DavidJoao <davidsandoval596@gmail.com>
2023-09-21 10:32:09 -0700

    CREATE Backend route to edit equipment if is not in use

---
+++ src/pages/api/equipment/edit/[equipmentId].js
@@ -0,0 +1,25 @@
+const Jobsite = require('../../../../models/jobsite');
+const Equipment = require('../../../../models/equipment');
+
+export default async function editEquipment (req, res, next) {
+
+    try {
+        const foundEquipment = await Equipment.findOne({ _id: req.query.equipmentId })
+        const foundJobsite = await Jobsite.findOne({ "equipment._id": foundEquipment._id })
+    
+        if (foundJobsite !== undefined) {
+            res.status(401).json({ message: `Remove equipment from ${foundJobsite.name} before editing` })
+        }
+
+        Object.assign(foundEquipment, req.body)
+        await foundEquipment.save()
+
+        res.json(foundEquipment)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Internal Error' })
+    }
+
+
+}
