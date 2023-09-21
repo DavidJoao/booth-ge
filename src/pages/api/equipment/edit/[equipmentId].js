47a490e6d7e11d8276efb717df972b4ea5eceeb0
DavidJoao <davidsandoval596@gmail.com>
2023-09-21 11:03:37 -0700

    FIX Bug when editing equipment

--- src/pages/api/equipment/edit/[equipmentId].js
+++ src/pages/api/equipment/edit/[equipmentId].js
@@ -7,14 +7,14 @@ export default async function editEquipment (req, res, next) {
         const foundEquipment = await Equipment.findOne({ _id: req.query.equipmentId })
         const foundJobsite = await Jobsite.findOne({ "equipment._id": foundEquipment._id })
     
-        if (foundJobsite !== undefined) {
+        if (foundJobsite !== null) {
             res.status(401).json({ message: `Remove equipment from ${foundJobsite.name} before editing` })
+        } else {
+            Object.assign(foundEquipment, req.body)
+            await foundEquipment.save()
+    
+            res.json(foundEquipment)
         }
-
-        Object.assign(foundEquipment, req.body)
-        await foundEquipment.save()
-
-        res.json(foundEquipment)
 
     } catch (error) {
         console.error(error)
