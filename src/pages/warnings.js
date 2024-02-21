257bc3c975a3357cc8b426f0c8aaee6d41da4336
DavidJoao <davidsandoval596@gmail.com>
2024-02-21 13:48:49 -0800

    CREATE Warning section

---
+++ src/pages/warnings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const warnings = () => {
+  return (
+    <div className='flex flex-col items-center bg-[#242526] h-[1200px] lg:h-screen pt-[85px] pb-2'>
+        warnings
+    </div>
+  )
+}
+
+export default warnings
