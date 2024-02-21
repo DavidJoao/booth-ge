84a7b4a24b4d9966086fe1bcfb38595f6f3ce1fc
DavidJoao <davidsandoval596@gmail.com>
2024-02-21 14:24:38 -0800

    CREATE Page section for incident reports

---
+++ src/pages/incidents.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const incidents = () => {
+  return (
+    <div className='flex flex-col items-center bg-[#242526] h-[1200px] lg:h-screen pt-[85px] pb-2'>
+        incidents
+    </div>
+  )
+}
+
+export default incidents
