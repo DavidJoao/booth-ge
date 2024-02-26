3ebfb0086c15808768fbed3691c4fac0dfefe51c
DavidJoao <davidsandoval596@gmail.com>
2024-02-26 09:05:57 -0800

    CREATE All primary functions for incidents

--- src/pages/incidents.js
+++ src/pages/incidents.js
@@ -21,6 +21,10 @@ const incidents = () => {
 	const { auth, setAuth, users } = useContext(AuthContext)
 	const [isLoading, setIsLoading] = useState(false)
 	const [statusMessage, setStatusMessage] = useState("Submitting Report, Please Wait...")
+	const [tempName, setTempName] = useState('')
+	const [involved, setInvolved] = useState([])
+	const [witnesses, setWitnesses] = useState([])
+	const [tempWitness, setTempWitness] = useState('')
 
 	const initialReport = {
 		name: "",
@@ -46,15 +50,42 @@ const incidents = () => {
 		otherNotes: "",
 	}
 
-	const [report, setReport] = useState(initialReport)
+	const initialTempInvolved = {
+		name: '',
+		employeeOrVisitor: '',
+		role: '',
+		supervisor: ''
+	}
 
-	const handleChange = e => {
+	const [report, setReport] = useState(initialReport)
+	const [tempInvolved, setTempInvolved] = useState(initialTempInvolved)
+
+	const handleChange = (e) => {
 		const { name, value } = e.target
 
 		setReport({
 			...report,
 			[name]: value,
+		})	
+		console.log(report)
+	}
+
+	const handleTempChange = (e) => {
+		const { name, value } = e.target;
+
+		setTempInvolved({
+			...tempInvolved,
+			[name]: value
 		})
+	}
+
+	const addToList = (e, list, setList, value) => {
+		e.preventDefault();
+
+		setList(prevArray => [...prevArray, value])
+		setTempWitness('')
+
+		console.log(list)
 	}
 
 	const handleSubmit = e => {
@@ -88,10 +119,10 @@ const incidents = () => {
 			) : (
 				<div>
 					<p className="text-xl font-bold">Incident Report Form</p>
-					<form className="w-screen border xl:w-[1200px] flex items-center justify-center flex-col gap-2 lg:grid lg:grid-cols-4 p-2" onSubmit={handleSubmit}>
+					<form className="w-screen border xl:w-[900px] flex items-start pl-5 justify-center flex-col gap-2" onSubmit={handleSubmit}>
 
 						<label>Type of Incident:</label>
-						<select value={report.type} name="type" required className="input">
+						<select value={report.type} name="type" required className="input" onChange={handleChange}>
 							<option>Select Type</option>
 							<option>Injury</option>
 							<option>Property Damage</option>
@@ -101,79 +132,121 @@ const incidents = () => {
 						</select>
 
 						<label>Date of Incident:</label>
-						<input value={report.date} name="date" type="date" className="input"/>
+						<input value={report.date} name="date" type="date" className="input" onChange={handleChange}/>
 
 						<label>Time of Incident:</label> 
-						<input value={report.time} name="time" type="time" className="input"/>
+						<input value={report.time} name="time" type="time" className="input" onChange={handleChange}/>
 
 						<label>Location of Incident:</label>
-						<input value={report.location} name="location" placeholder="" className="input"/>
+						<input value={report.location} name="location" placeholder="" className="input" onChange={handleChange}/>
 
+
+						{/* MAKE THIS EDITABLE */}
 						<label>Involved Individual</label>
-						<input value={report.name} name="name" className="input"/>
+						<select id="name" value={report.name} name="name" className='input' onChange={(e) => {
+							handleChange(e)
+							if(e.target.value === 'Other') {
+								setTempName('Other')
+							}
+							
+							}}>
+							<option>Choose Employee/Other</option>
+							<option>Other</option>
+							{ users && users.map((user) => {
+								return (
+									<option key={user._id}>{user.name}</option>
+								)
+							}) }
+						</select>
+						<input value={report.name} placeholder="Specify" name="name" className={tempName === 'Other' ? 'input' : 'hidden'} onChange={handleChange}/>
 
 						<label>Employee or Visitor?</label>
-						<select value={report.employeeType} name="employeeType" required className="input">
+						<select value={report.employeeType} name="employeeType" required className="input" onChange={handleChange}>
 							<option>Select Answer</option>
 							<option>Employee</option>
 							<option>Rental</option>
 						</select>
 
 						<label>Role:</label>
-						<input value={report.role} name="role" placeholder="Ex: Foreman, Laborer" className="input"/>
+						<input value={report.role} name="role" placeholder="Ex: Foreman, Laborer" className="input" onChange={handleChange}/>
 
 						<label>Supervisor:</label>
-						<input value={report.supervisor} name="supervisor" className="input"/>
+						<input value={report.supervisor} name="supervisor" className="input" onChange={handleChange}/>
 
 						{/* USE TYPE -> BUTTON -> PUSH TO ARRAY  */}
 						{/* MORE INVOLVED PEOPLE */}
+						<label>More Involved Individuals</label>
+						<div className="grid grid-cols-2 gap-2 items-center mt-4 mb-4">
+							<label>Name:</label>
+							<input className="input"/>
+							<label>Employee or Visitor?</label>
+							<select className="input" >
+								<option>Yes</option>
+								<option>No</option>
+							</select>
+							<label>Role</label>
+							<input className="input" />
+							<label>Supervisor</label>
+							<input className="input" />
+							<button className="buttons w-[150px]">Add</button>
+						</div>
 						{/* WITNESSES */}
+						<label>Witnesses:</label>
+						<input className="input" required value={tempWitness} placeholder="Name" onChange={(e) => setTempWitness(e.target.value)}/>
+						<button onClick={(e) => {
+							if(tempWitness !== '') addToList(e, witnesses, setWitnesses, tempWitness)
+							}} className="buttons w-[150px]">Add Witness</button>
+						<ol className="list-decimal">
+							{ witnesses && witnesses.map((witness) => {
+								return (
+									<li>{witness}</li>
+								)
+							}) }
+						</ol>
 
 						<label>Description of Incident:</label>
-						<textarea value={report.description} name="description" className="input h-[150px] resize-none"/>
+						<textarea value={report.description} name="description" className="input h-[150px] resize-none" onChange={handleChange}/>
 
 						<label>Injuries/Losses:</label>
-						<textarea value={report.injuriesOrLosses} name="injuriesOrLosses" className="input h-[150px] resize-none"/>
+						<textarea value={report.injuriesOrLosses} name="injuriesOrLosses" className="input h-[150px] resize-none" onChange={handleChange}/>
 
 						<label>Hospitalization Required? </label>
-						<select value={report.hospitalRequired} name="hospitalRequired" className="input">
+						<select value={report.hospitalRequired} name="hospitalRequired" className="input" onChange={handleChange}>
 							<option>Select Answer</option>
 							<option>Yes</option>
 							<option>No</option>
 						</select>
 
 						<label>Physician Required? </label>
-						<select value={report.physicianRequired} name="physicianRequired" required className="input">
+						<select value={report.physicianRequired} name="physicianRequired" required className="input" onChange={handleChange}>
 							<option>Select Answer</option>
 							<option>Yes</option>
 							<option>No</option>
 						</select>
 
 						<label>Facility Name:</label>
-						<input value={report.facilityName} name="facilityName" className="input" />
+						<input value={report.facilityName} name="facilityName" className="input" onChange={handleChange}/>
 
 						<label>Law Enforcement Required?</label>
-						<select value={report.enforcementRequired} name="enforcementRequired" className="input">
+						<select value={report.enforcementRequired} name="enforcementRequired" className="input" onChange={handleChange}>
 							<option>Select Answer</option>
 							<option>Yes</option>
 							<option>No</option>
 						</select>
 
 						<label>Date and Time of Contact:</label>
-						<input value={report.enforcementTime} name="enforcementTime" type="datetime-local" className="input" />
+						<input value={report.enforcementTime} name="enforcementTime" type="datetime-local" className="input"  onChange={handleChange}/>
 
 						<label>Law Enforcement Agency Name:</label>
-						<input value={report.enforcementName} name="enforcementName" className="input" />
+						<input value={report.enforcementName} name="enforcementName" className="input"  onChange={handleChange}/>
 
 						<label>Report Number:</label>
-						<input value={report.reportId} name="reportId" className="input" />
+						<input value={report.reportId} name="reportId" className="input"  onChange={handleChange}/>
 
 						<label>Other Notes:</label>
-						<textarea value={report.otherNotes} name="otherNotes" className="input resize-none" />
+						<textarea value={report.otherNotes} name="otherNotes" className="input resize-none"  onChange={handleChange}/>
 
-						<div className="col-span-4 flex items-center justify-center p-2">
-							<button type="submit" className="buttons mt-3 w-[150px] mx-auto">Submit Form</button>
-						</div>
+							<button type="submit" className="buttons mt-3 mb-3 w-[150px] mx-auto">Submit Form</button>
 
 					</form>
 				</div>
