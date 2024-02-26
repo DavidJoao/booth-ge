5a431f3b9ce9ef57adcfbc44b695c7dd6a37f65c
DavidJoao <davidsandoval596@gmail.com>
2024-02-26 15:23:37 -0800

    CREATE Half of page for incident reports pdf creation

--- src/pages/api/email/incident.js
+++ src/pages/api/email/incident.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer')
 import jsPDF from 'jspdf';
+const fs = require('fs')
 
 export default async function sendIncidentReport (req, res, next) {
 
@@ -8,12 +9,56 @@ export default async function sendIncidentReport (req, res, next) {
     const doc = new jsPDF();
 
     /////////////////////////////////////////////////////////
+    
+    doc.line(doc.internal.pageSize.getWidth() / 2, 15, doc.internal.pageSize.getWidth() / 2, doc.internal.pageSize.getHeight())
 
-    doc.setFont(undefined, 'bold');
+    // IN CASE OF REQUIRED LOGO
+    // const imageBuffer = fs.readFileSync('assets/boothimg.jpeg')
+    // const imageArray = new Uint8Array(imageBuffer)
+    // doc.addImage(imageArray, 'JPEG', doc.internal.pageSize.getWidth() - 20, 10, 10, 10);
+
+    doc.setFont(undefined, 'bold')
     doc.setFontSize(14)
     doc.text(`Booth Grading and Excavating: Incident Report Form`, 10, 10)
     doc.setFontSize(10)
     doc.setFont(undefined, 'normal')
+
+    doc.text(`Type of Incident: ${form.type}`, 10, 20)
+    doc.text(`Date: ${form.date || 'N/A'}`, 10, 27)
+    doc.text(`Time: ${date.time || 'N/A'}`, 40, 27)
+    doc.text(`Name: ${form.name}`, 10, 34)
+    doc.text(`Employee or Visitor? ${form.employeeType}`, 10, 41)
+    doc.text(`Role: ${form.role}`, 10, 48)
+    doc.text(`Supervisor: ${form.supervisor}`, 10, 55)
+    doc.text(`Location: ${form.location}`, 10, 62)
+
+    doc.setFont(undefined, 'bold')
+    doc.text(`Description of Incident: `, 10, 77)
+    doc.setFont(undefined, 'normal')
+    const descriptionLines = doc.splitTextToSize(form.description, (doc.internal.pageSize.getWidth() / 2) - 10)
+    descriptionLines.forEach((line, index) => {
+        doc.text(line, 10, 84 + index * 5)
+    })
+
+    doc.setFont(undefined, 'bold')
+    doc.text(`Injuries/Losses: `, 10, 147)
+    doc.setFont(undefined, 'normal')
+    const injuriesLines = doc.splitTextToSize(form.injuriesOrLosses, (doc.internal.pageSize.getWidth() / 2) - 10)
+    injuriesLines.forEach((line, index) => {
+        doc.text(line, 10, 154 + index * 5)
+    })
+
+    doc.text(`Hospitalization Required? ${form.hospitalRequired}`, 10, 204)
+    doc.text(`Physician Required? ${form.physicianRequired}`, 10, 211)
+    doc.text(`Facility Name: ${form.facilityName || 'N/A'}`, 10, 218)
+    
+    doc.text(`Law Enforcement Contact Required? ${form.enforcementRequired}`, 10, 228)
+    const [year, month, day] = form.enforcementTime.split('T')[0].split('-')
+    const time = form.enforcementTime.split('T')[1]
+    doc.text( form.enforcementRequired === 'Yes' ? `Date and Time of Contact: ${month}-${day}-${year} at ${time}` : 'Date and Time of Contact: N/A', 10, 235)
+    doc.text(`Law Enforcement Agency Name:`, 10, 242)
+    doc.text(form.enforcementRequired === 'Yes' ? `- ${form.enforcementName}`: 'N/A', 10, 247)
+    doc.text( form.enforcementRequired === 'Yes' ? `Report Number (if applicable): ${form.reportId}` : 'Report Number (if applicable): N/A', 10, 254)
 
     /////////////////////////////////////////////////////////
 
@@ -31,11 +76,11 @@ export default async function sendIncidentReport (req, res, next) {
     const mailOptions = {
         from: "boothpaperwork@hotmail.com",
         to: "davidsandoval596@gmail.com",
-        subject: `Incident Report Form - ${form.submittedBy}`,
+        subject: `Incident Report Form`,
         text: ``,
         attachments: [
             {
-                filename: `Incident Report Form - ${form.submittedBy}`,
+                filename: `Incident Report Form`,
                 content: buffer,
                 contentType: "application/pdf",
             },
