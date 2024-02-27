1e6630b836179a1d2733ee271352b4b5e3dc8fca
DavidJoao <davidsandoval596@gmail.com>
2024-02-27 11:05:45 -0800

    FINISH Incident backend configuration

--- src/pages/api/email/incident.js
+++ src/pages/api/email/incident.js
@@ -13,9 +13,9 @@ export default async function sendIncidentReport (req, res, next) {
     doc.line(doc.internal.pageSize.getWidth() / 2, 15, doc.internal.pageSize.getWidth() / 2, doc.internal.pageSize.getHeight())
 
     // IN CASE OF REQUIRED LOGO
-    // const imageBuffer = fs.readFileSync('assets/boothimg.jpeg')
-    // const imageArray = new Uint8Array(imageBuffer)
-    // doc.addImage(imageArray, 'JPEG', doc.internal.pageSize.getWidth() - 20, 10, 10, 10);
+    const imageBuffer = fs.readFileSync('assets/boothimg.jpeg')
+    const imageArray = new Uint8Array(imageBuffer)
+    doc.addImage(imageArray, 'JPEG', doc.internal.pageSize.getWidth() - 20, doc.internal.pageSize.getHeight() - 20, 10, 10);
 
     doc.setFont(undefined, 'bold')
     doc.setFontSize(14)
@@ -23,14 +23,27 @@ export default async function sendIncidentReport (req, res, next) {
     doc.setFontSize(10)
     doc.setFont(undefined, 'normal')
 
+    // PRIMARY INFORMATION SECTION
+
     doc.text(`Type of Incident: ${form.type}`, 10, 20)
     doc.text(`Date: ${form.date || 'N/A'}`, 10, 27)
-    doc.text(`Time: ${date.time || 'N/A'}`, 40, 27)
+    doc.text(`Time: ${form.time || 'N/A'}`, 40, 27)
     doc.text(`Name: ${form.name}`, 10, 34)
     doc.text(`Employee or Visitor? ${form.employeeType}`, 10, 41)
     doc.text(`Role: ${form.role}`, 10, 48)
     doc.text(`Supervisor: ${form.supervisor}`, 10, 55)
     doc.text(`Location: ${form.location}`, 10, 62)
+
+    // OTHER INVOLVED INDIVIDUALS SECTION
+
+    doc.setFont(undefined, 'bold')
+    doc.text(`Other involved individuals:`, (doc.internal.pageSize.getWidth() / 2) + 10, 20)
+    doc.setFont(undefined, 'normal')
+    form.involved.forEach((employee, index) => {
+        doc.text(`- ${`${employee.name} | ${employee.employeeOrVisitor} | ${employee.role}` || ''}`, (doc.internal.pageSize.getWidth() / 2) + 10, 25 + index * 5)
+     })
+
+    //  DESCRIPTION OF INCIDENT
 
     doc.setFont(undefined, 'bold')
     doc.text(`Description of Incident: `, 10, 77)
@@ -40,6 +53,13 @@ export default async function sendIncidentReport (req, res, next) {
         doc.text(line, 10, 84 + index * 5)
     })
 
+    // WITNESSES
+
+    doc.setFont(undefined, 'bold')
+    doc.text("Witnesses:", (doc.internal.pageSize.getWidth() / 2) + 10, 77)
+    doc.setFont(undefined, 'normal')
+    doc.text(`${form.witnesses}`, (doc.internal.pageSize.getWidth() / 2) + 10, 84)
+
     doc.setFont(undefined, 'bold')
     doc.text(`Injuries/Losses: `, 10, 147)
     doc.setFont(undefined, 'normal')
@@ -47,6 +67,17 @@ export default async function sendIncidentReport (req, res, next) {
     injuriesLines.forEach((line, index) => {
         doc.text(line, 10, 154 + index * 5)
     })
+
+    // OTHER NOTES
+    doc.setFont(undefined, 'bold')
+    doc.text(`Other Notes: `, (doc.internal.pageSize.getWidth() / 2) + 10, 147)
+    doc.setFont(undefined, 'normal')
+    const otherNotesLines = doc.splitTextToSize(form.otherNotes, (doc.internal.pageSize.getWidth() / 2) - 20)
+    otherNotesLines.forEach((line, index) => {
+        doc.text(line, (doc.internal.pageSize.getWidth() / 2) + 10, 154 + index * 5)
+    })
+
+    // HOSPITALIZATION OR ENFROCEMENT CONTACT
 
     doc.text(`Hospitalization Required? ${form.hospitalRequired}`, 10, 204)
     doc.text(`Physician Required? ${form.physicianRequired}`, 10, 211)
@@ -59,6 +90,9 @@ export default async function sendIncidentReport (req, res, next) {
     doc.text(`Law Enforcement Agency Name:`, 10, 242)
     doc.text(form.enforcementRequired === 'Yes' ? `- ${form.enforcementName}`: 'N/A', 10, 247)
     doc.text( form.enforcementRequired === 'Yes' ? `Report Number (if applicable): ${form.reportId}` : 'Report Number (if applicable): N/A', 10, 254)
+
+    doc.text(`Submitted By: ${form.submittedBy}`, 10, doc.internal.pageSize.getHeight() - 10)
+
 
     /////////////////////////////////////////////////////////
 
