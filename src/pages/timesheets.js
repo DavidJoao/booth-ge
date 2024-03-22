b3484bd13c8a1395cb5ca819230b021559aeb5d4
DavidJoao <davidsandoval596@gmail.com>
2024-03-21 19:44:49 -0700

    ADD Button and functionality to get more results in timesheets

--- src/pages/timesheets.js
+++ src/pages/timesheets.js
@@ -6,10 +6,11 @@ import CheckSession from '@/custom/CheckSession'
 import TimesheetCard from '@/components/TimesheetCard'
 import Link from 'next/link'
 import CCSection from '@/components/CCSection'
+import axios from '@/custom/axios'
 
 const Timesheets = () => {
 
-    const { auth, setAuth, loadAll, timesheets } = useContext(AuthContext)
+    const { auth, setAuth, loadAll, timesheets, setTimesheetCount, timesheetCount, setTimesheets } = useContext(AuthContext)
     const router = useRouter()
     const [search, setSearch] = useState('')
 
@@ -22,6 +23,17 @@ const Timesheets = () => {
       CheckSession(AuthContext, setAuth)
     }, [])
 
+    const handleMoreResults = async (e) => {
+      e.preventDefault();
+
+      setTimesheetCount(prevCount => {
+        const newCount = prevCount + 5;
+        axios.get(`/api/timesheet/all/${newCount}`)
+          .then(res => setTimesheets(res.data));
+        return newCount;
+      });
+    }
+
   return (
     <div className="bg-[#242526] min-h-screen h-auto lg:h-screen flex flex-col items-center justify-center pt-[80px]">
       { auth.isModerator ? 
@@ -32,8 +44,9 @@ const Timesheets = () => {
         </div>
         <div className='w-full lg:w-[90%] h-auto p-3 pt-0'>
             { timesheets && timesheets.filter( timesheet => 
-              search === '' || timesheet.author.toLowerCase().includes(search.toLowerCase())).reverse().map(timesheet => <TimesheetCard key={`${timesheet._id}${timesheet.author}${timesheet.dateCreated}`} loadAll={loadAll} timesheet={timesheet} auth={auth}/>) }
+              search === '' || timesheet.author.toLowerCase().includes(search.toLowerCase())).map(timesheet => <TimesheetCard key={`${timesheet._id}${timesheet.author}${timesheet.dateCreated}`} loadAll={loadAll} timesheet={timesheet} auth={auth}/>) }
         </div>
+        <button className='buttons w-[300px]' onClick={handleMoreResults}> Load 5 more results | Currently {timesheetCount} </button>
       </>
       :
       <>
