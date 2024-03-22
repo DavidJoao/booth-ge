68361592295353fbd63adbae51b4c17ae5261900
DavidJoao <davidsandoval596@gmail.com>
2024-03-21 19:45:09 -0700

    DELETE Past backend files to create new ones with params

--- src/pages/api/daily/all.js
+++
@@ -1,7 +0,0 @@
-const Daily = require('../../../models/daily')
-
-export default async function allDailies (req, res, next) {
-    await Daily.find({})
-    .then(daily => res.json(daily))
-    .catch(next)
-}
--- src/pages/api/timesheet/all.js
+++
@@ -1,7 +0,0 @@
-const Timesheet = require('../../../models/timesheet')
-
-export default async function allTimesheets (req, res, next) {
-    await Timesheet.find({})
-        .then(timesheets => res.json(timesheets))
-        .catch(next)
-}
