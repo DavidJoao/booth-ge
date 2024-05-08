d18c843faa9afbe120940d4626d96f8bf03be27e
DavidJoao <davidsandoval596@gmail.com>
2024-05-08 15:48:55 -0700

    FIXED EMAIL, ADDED BGEPAYROLL TO DESTINATARY

--- src/pages/api/uploads/docs.js
+++ src/pages/api/uploads/docs.js
@@ -44,7 +44,7 @@ export default async function sendDocuments(req, res) {
       
       transporter.sendMail( {
         from: 'boothpaperwork@hotmail.com',
-        to: 'davidsandoval596@gmail.com',
+        to: 'bgepayroll@gmail.com',
         subject: filename,
         text: '',
         attachments: [
